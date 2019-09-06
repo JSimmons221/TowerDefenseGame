f32e0c5cbd09984893f71674f1288db2d3953040
jakob_simmons <JSimmons01778@gmail.com>
2019-09-06 13:34:56 -0400

    Path and Tower Tile

--- src/TowerTile.java
+++ src/TowerTile.java
@@ -1,20 +1,19 @@
 import java.awt.*;
 
-public class Path extends Tile{
+public class TowerTile extends Tile{
 
-    boolean walled;
+    boolean filled;
     int x,y;
 
-    public Path (int x, int y){
-        walled = false;
+    public TowerTile (int x, int y){
+        filled=false;
         this.x=x;
         this.y=y;
     }
 
     @Override
     void draw(Graphics2D g2) {
-        g2.setColor(new Color(245,222,179));
+        g2.setColor(new Color(34,149,34));
         g2.drawRect(x,y,Main.TWIDTH,Main.TWIDTH);
     }
-
 }
